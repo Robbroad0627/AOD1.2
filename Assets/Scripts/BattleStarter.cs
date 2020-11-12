d964cfce2288f37f71a92f21c7326df8d7e0406a
Rob Broad <robbroad0627@gmail.com>
2020-11-12 02:55:44 -0500

    Mandu added

--- Assets/Scripts/BattleStarter.cs
+++ Assets/Scripts/BattleStarter.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class BattleStarter : MonoBehaviour {
 
-    public BAttleType[] potentialBattles;
+    public BattleType[] potentialBattles;
 
     public bool activateOnEnter, activateOnStay, activateOnExit;
 
