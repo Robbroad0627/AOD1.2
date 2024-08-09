ad7950e94e2585fcbcb7aabf69e0793776d6d906
CrazyTobi <129459033+CrazyTobi@users.noreply.github.com>
2024-08-09 17:29:24 -0400

    Manual Control Boat Update 3
    Got the animation for the boat in the world map working

---
+++ Assets/Scripts/WorldBoat.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Bonehead Games
+
+public class WorldBoat : MonoBehaviour
+{
+    private Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            m_Animator.SetBool("FacingRight", true);
+            m_Animator.SetBool("FacingLeft", false);
+            m_Animator.SetBool("FacingUp", false);
+            m_Animator.SetBool("FacingDown", false);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            m_Animator.SetBool("FacingRight", false);
+            m_Animator.SetBool("FacingLeft", true);
+            m_Animator.SetBool("FacingUp", false);
+            m_Animator.SetBool("FacingDown", false);
+        }
+
+        if (Input.GetAxisRaw("Vertical") == 1)
+        {
+            m_Animator.SetBool("FacingRight", false);
+            m_Animator.SetBool("FacingLeft", false);
+            m_Animator.SetBool("FacingUp", true);
+            m_Animator.SetBool("FacingDown", false);
+        }
+
+        if (Input.GetAxisRaw("Vertical") == -1)
+        {
+            m_Animator.SetBool("FacingRight", false);
+            m_Animator.SetBool("FacingLeft", false);
+            m_Animator.SetBool("FacingUp", false);
+            m_Animator.SetBool("FacingDown", true);
+        }
+    }
+}
