c3d14ab893cbbbc5f839f7f0878f3f2a55efc27b
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2024-08-23 10:30:24 -0400

    Update Script

--- Assets/Scripts/Exits & Entrances/AreaExit.cs
+++ Assets/Scripts/Exits & Entrances/AreaExit.cs
@@ -1,55 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/****************************************************************************************
+ * Copyright: Bonehead Games
+ * Script: AreaExit.cs
+ * Date Created: 
+ * Created By: Rob Broad
+ * Description:
+ * **************************************************************************************
+ * Modified By: Jeff Moreau
+ * Date Last Modified: August 23, 2024
+ * TODO: Variables should NEVER be public
+ * Known Bugs: 
+ ****************************************************************************************/
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//Bonehead Games
+public class AreaExit : MonoBehaviour
+{
+    //VARIABLES
+    #region Inspector/Exposed Variables
 
-public class AreaExit : MonoBehaviour {
-
+    // Do NOT rename SerializeField Variables or Inspector exposed Variables
+    // unless you know what you are changing
+    // You will have to reenter all values in the inspector to ALL Objects that
+    // reference this script.
+    // The Proper way to expose variables to the editor
+    // without exposing them outside of this script
+    // [SerializeField] private variable variableName = initialvalue;
+    // Example:
+    // [SerializeField] private float health = 10.0f;
     public string areaToLoad;
     public string areaTransitionName;
-   
     public float waitToLoad = 1f;
-
     public PortController portController;
+    public bool needBoat = false;
+
+    #endregion
+    #region Private Variable Declarations Only
 
     private bool shouldLoadAfterFade;
     private bool shouldRunAnimationBeforeFade;
 
-    public bool needBoat = false;
+    #endregion
 
-	void Start () {
-        
-    }
-	
-	void Update () {
-		if(shouldLoadAfterFade)
-        {
-            waitToLoad -= Time.deltaTime;
-            if(waitToLoad <= 0)
-            {
-                shouldLoadAfterFade = false;
-                SceneManager.LoadScene(areaToLoad);
-            }
-        }
-
-        if (shouldRunAnimationBeforeFade)
-        {
-            if (Boat.boatLeftPort)
-            {
-                this.enabled = true;//Be sure we are enabled or we won't get updates and the next scene will never load.
-                                    //SceneManager.LoadScene(areaToLoad);
-                shouldLoadAfterFade = true;
-                GameManager.instance.fadingBetweenAreas = true;
-
-                UIFade.instance.FadeToBlack();
-
-                PlayerController.instance.areaTransitionName = areaTransitionName;
-                shouldRunAnimationBeforeFade = false;
-            }
-        }
-    }
+    //FUNCTIONS
+    #region Physics Methods/Functions
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -79,4 +73,39 @@ public class AreaExit : MonoBehaviour {
             }
         }
     }
+
+    #endregion
+    #region Implementation Private Methods/Functions
+
+    private void Update ()
+    {
+		if (shouldLoadAfterFade)
+        {
+            waitToLoad -= Time.deltaTime;
+
+            if(waitToLoad <= 0)
+            {
+                shouldLoadAfterFade = false;
+                SceneManager.LoadScene(areaToLoad);
+            }
+        }
+
+        if (shouldRunAnimationBeforeFade)
+        {
+            if (Boat.boatLeftPort)
+            {
+                this.enabled = true;//Be sure we are enabled or we won't get updates and the next scene will never load.
+                                    //SceneManager.LoadScene(areaToLoad);
+                shouldLoadAfterFade = true;
+                GameManager.instance.fadingBetweenAreas = true;
+
+                UIFade.instance.FadeToBlack();
+
+                PlayerController.instance.areaTransitionName = areaTransitionName;
+                shouldRunAnimationBeforeFade = false;
+            }
+        }
+    }
+
+    #endregion
 }
