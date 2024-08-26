7c7c4aad15721a1d835b9ec891cceaeff85e9468
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2024-08-26 11:58:03 -0400

    DialogActivator Update

--- Assets/Scripts/Dialog/DialogActivator.cs
+++ Assets/Scripts/Dialog/DialogActivator.cs
@@ -1,49 +1,78 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/****************************************************************************************
+ * Copyright: Bonehead Games
+ * Script: DialogActivator.cs
+ * Date Created: 
+ * Created By: Rob Broad
+ * Description:
+ * **************************************************************************************
+ * Modified By: Jeff Moreau
+ * Date Last Modified: August 26, 2024
+ * TODO: Variables should NEVER be public
+ * Known Bugs: 
+ ****************************************************************************************/
+
 using UnityEngine;
 
-//Bonehead Games
+public class DialogActivator : MonoBehaviour
+{
+    //VARIABLES
+    #region Inspector/Exposed Variables
 
-public class DialogActivator : MonoBehaviour {
+    // Do NOT rename SerializeField Variables or Inspector exposed Variables
+    // unless you know what you are changing
+    // You will have to reenter all values in the inspector to ALL Objects that
+    // reference this script.
+    [SerializeField] private string[] lines = null;
+    [SerializeField] private bool isPerson = true;
+    [SerializeField] private bool shouldActivateQuest = false;
+    [SerializeField] private string questToMark = "";
+    [SerializeField] private bool markComplete = false;
 
-    public string[] lines;
+    #endregion
+    #region Private Variables
 
-    private bool canActivate;
+    private bool mCanActivate;
 
-    public bool isPerson = true;
+    #endregion
 
-    public bool shouldActivateQuest;
-    public string questToMark;
-    public bool markComplete;
+    //FUNCTIONS
+    #region Initialization Functions/Methods
 
-
-	// Use this for initialization
-	void Start () {
-		
+    private void Start ()
+    {
+        mCanActivate = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(canActivate && Input.GetButtonDown("Fire1") && !DialogManager.instance.dialogBox.activeInHierarchy)
+
+    #endregion
+    #region Physics Functions/Methods
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            mCanActivate = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            mCanActivate = false;
+        }
+    }
+
+    #endregion
+    #region Implementation Functions/Methods
+
+    private void Update ()
+    {
+		if (mCanActivate && Input.GetButtonDown("Fire1") && !DialogManager.instance.dialogBox.activeInHierarchy)
         {
             DialogManager.instance.ShowDialog(lines, isPerson);
             DialogManager.instance.ShouldActivateQuestAtEnd(questToMark, markComplete);
         }
 	}
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.tag == "Player")
-        {
-            canActivate = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "Player")
-        {
-            canActivate = false;
-        }
-    }
+    #endregion
 }
