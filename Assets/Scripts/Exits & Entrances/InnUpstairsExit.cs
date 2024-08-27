947b2a77506747837b2c6b8023b11987c30f1280
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2024-08-27 10:34:01 -0400

    InnUpstairsExit Update

--- Assets/Scripts/Exits & Entrances/InnUpstairsExit.cs
+++ Assets/Scripts/Exits & Entrances/InnUpstairsExit.cs
@@ -1,52 +1,93 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/****************************************************************************************
+ * Copyright: Bonehead Games
+ * Script: InnUpstairsExit.cs
+ * Date Created: 
+ * Created By: Rob Broad
+ * Description:
+ * **************************************************************************************
+ * Modified By: Jeff Moreau
+ * Date Last Modified: August 27, 2024
+ * TODO: Variables should NEVER be public
+ * Known Bugs: 
+ ****************************************************************************************/
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class InnUpstairsExit :MonoBehaviour
 {
-    [HideInInspector]
-    public string areaToLoad => Inn.GetDownstairsSceneName;
-    [HideInInspector]
-    public string areaTransitionName => Inn.GetDownstairsTransitionName;
+    //VARIABLES
+    #region Constant Variable Declarations and Initializations
 
-    public float waitToLoad = 1f;
-    private bool shouldLoadAfterFade;
+    private const string PLAYER = "Player";
 
-    public bool needBoat = false;
+    #endregion
+    #region Inspector/Exposed Variables
 
-    void Update()
+    // Do NOT rename SerializeField Variables or Inspector exposed Variables
+    // unless you know what you are changing
+    // You will have to reenter all values in the inspector to ALL Objects that
+    // reference this script.
+    [SerializeField] private float waitToLoad = 1f;
+    [SerializeField] private bool needBoat = false;
+
+    #endregion
+    #region Private Variables
+
+    private string mAreaToLoad;
+    private bool mDoesLoadAfterFade;
+    private string mAreaTransitionName;
+
+    #endregion
+
+    //FUNCTIONS
+    #region Initialization Functions/Methods
+
+    private void Start()
     {
-        if (shouldLoadAfterFade)
+        mAreaToLoad = Inn.GetDownstairsSceneName;
+        mAreaTransitionName = Inn.GetDownstairsTransitionName;
+    }
+
+    #endregion
+    #region Implementation Functions/Methods
+
+    private void Update()
+    {
+        if (mDoesLoadAfterFade)
         {
             waitToLoad -= Time.deltaTime;
+
             if (waitToLoad <= 0)
             {
-                shouldLoadAfterFade = false;
-                SceneManager.LoadScene(areaToLoad);
+                mDoesLoadAfterFade = false;
+                SceneManager.LoadScene(mAreaToLoad);
                 Inn.SetIsPlayerUpstairs(false);
             }
         }
     }
 
+    #endregion
+    #region Physics Functions/Methods
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.CompareTag(PLAYER))
         {
             if (needBoat && !GameManager.instance.GetHasBoat)
             {
                 //Cant use boat area without one.
-                Debug.Log("Area needs boat but GameManager.haveBoat == false");
+                Debug.Log("Area needs boat but GameManager.GetHasBoat == false");
                 return;
             }
-            //PlayerController.instance.areaTransitionName = Inn.s_downstairsTransitionName;
-            this.enabled = true;//Be sure we are enabled or we won't get updates and the next scene will never load.
-            shouldLoadAfterFade = true;
+
+            enabled = true;//Be sure we are enabled or we won't get updates and the next scene will never load.
+            mDoesLoadAfterFade = true;
             GameManager.instance.SetFadingBetweenAreas(true);
 
             UIFade.instance.FadeToBlack();
-
-            
         }
     }
+
+    #endregion
 }
