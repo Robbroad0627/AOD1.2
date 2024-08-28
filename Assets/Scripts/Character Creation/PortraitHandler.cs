14a9ddaf90daa678707ec21a602c2e9d8f5ebe42
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2024-08-27 20:29:33 -0400

    PortraitHandler Update

--- Assets/Scripts/Character Creation/PortraitHandler.cs
+++ Assets/Scripts/Character Creation/PortraitHandler.cs
@@ -1,16 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿/****************************************************************************************
+ * Copyright: Bonehead Games
+ * Script: InnUpstairsExit.cs
+ * Date Created: 
+ * Created By: Rob Broad
+ * Description:
+ * **************************************************************************************
+ * Modified By: Jeff Moreau
+ * Date Last Modified: August 27, 2024
+ * TODO: Variables should NEVER be public
+ * Known Bugs: 
+ ****************************************************************************************/
 
-//Bonehead Games
+using UnityEngine;
+using System.Collections.Generic;
 
 public class PortraitHandler : MonoBehaviour
 {
-    public List<Sprite> mHuman, fHuman;
-    public List<Sprite> mDwarf, fDwarf;
-    public List<Sprite> mElf, fElf;
-    public List<Sprite> mHalf_Elf, fHalf_Elf;
-    public List<Sprite> mHalfling, fHalfling;
-    public List<Sprite> mHalf_Orc, fHalf_Orc;
+    //VARIABLES
+    #region Inspector/Exposed Variables
+
+    // Do NOT rename SerializeField Variables or Inspector exposed Variables
+    // unless you know what you are changing
+    // You will have to reenter all values in the inspector to ALL Objects that
+    // reference this script.
+    [SerializeField] private List<Sprite> mDwarf;
+    [SerializeField] private List<Sprite> fDwarf;
+    [SerializeField] private List<Sprite> mElf;
+    [SerializeField] private List<Sprite> fElf;
+    [SerializeField] private List<Sprite> mHalf_Elf;
+    [SerializeField] private List<Sprite> fHalf_Elf;
+    [SerializeField] private List<Sprite> mHalf_Orc;
+    [SerializeField] private List<Sprite> fHalf_Orc;
+    [SerializeField] private List<Sprite> mHalfling;
+    [SerializeField] private List<Sprite> fHalfling;
+    [SerializeField] private List<Sprite> mHuman;
+    [SerializeField] private List<Sprite> fHuman;
+
+    #endregion
+
+    //GETTERS
+    #region Getters/Accessors
+
+    public List<Sprite> GetDwarfMale => mDwarf;
+    public List<Sprite> GetDwarfFemale => fDwarf;
+    public List<Sprite> GetElfMale => mElf;
+    public List<Sprite> GetElfFemale => fElf;
+    public List<Sprite> GetHalfElfMale => mHalf_Elf;
+    public List<Sprite> GetHalfElfFemale => fHalf_Elf;
+    public List<Sprite> GetHalfOrcMale => mHalf_Orc;
+    public List<Sprite> GetHalfOrcFemale => fHalf_Orc;
+    public List<Sprite> GetHalflingMale => mHalfling;
+    public List<Sprite> GetHalflingFemale => fHalfling;
+    public List<Sprite> GetHumanMale => mHuman;
+    public List<Sprite> GetHumanFemale => fHuman;
+
+    #endregion
 }
